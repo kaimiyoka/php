d074b66404c6153412db787cb80a94cdc5570959
kaimiyoka <163088917+kaimiyoka@users.noreply.github.com>
2025-09-03 19:54:39 +0800

    Add files via upload

---
+++ htdocs/php_practice.php
@@ -0,0 +1,248 @@
+<!-- Hello Girl
+<h1>Hello!</h1>
+<style>
+    h1{
+        color:yellowgreen;
+    }
+</style>
+<script>
+    alert("Hello!")
+</script> -->
+<?php
+    // echo "<b>Hello Wolrd</b>";   
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <h1>Hello Everybodyng</h1> -->
+    <?php
+        /*echo "Hello";
+        
+        $color = "Red";
+        $Color = "Blue";
+
+        echo $color;
+        echo $Color;
+
+        $_ = 5;
+
+        echo $_;
+
+        var_dump($color);
+        var_dump($_);
+        var_dump(3.1);
+        var_dump(true);
+        var_dump([1,2])
+        
+        $x = 5;
+
+        function test(){
+            //$x = 10;
+            static $x = 10;
+            echo $x;
+            $x++;
+            echo "<br>";
+        }
+
+
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        test();
+        
+        echo $x;*/
+
+        /*echo ("Hello"), ("Hello"), ("Hello");
+        echo "World", "World";
+        print ("Hello");
+        print "World";*/
+
+        //$x = echo("Hello");
+        //echo $x,"";
+
+        /*$a = "Michael Jorkson";
+        $b = "5";
+        $c = "4.1";
+        $d = true;
+        $e = [1,2,3];
+        $f = new stdClass();
+        $g = null;
+        $h = fopen("php_practice.php", "r");
+
+        var_dump($a);
+        echo "";
+        var_dump($b);
+        echo "";
+        var_dump($c);
+        echo "";
+        var_dump($d);
+        echo "";
+        var_dump($e);
+        echo "";
+        var_dump($f);
+        echo "";
+        var_dump($g);
+        echo "";
+        var_dump($h);
+        echo "";
+
+        // echo strlen($a);
+        
+        $x= "Edwin Cruz";
+        echo str_replace("Cruz","Edwin", $x);
+        
+        echo trim("Hello        World!    ");
+        var_dump(explode("u", $x));*/
+
+        ////echo "Hello " . "World";
+
+        //$x = "Hello";
+       // $y = "Love";
+
+        //echo $x . " " . $y, "<br>";
+        //echo "$x $y <br>";
+        //echo "Juan \";
+
+       // $x = 10;
+        //define("_GREETING", "Hello World!");
+
+        //echo $x;
+        //var_dump( _GREETING);
+
+        //const _GREETING2 = "Hello People!";
+        //echo _GREETING2;
+        //function test(){
+       //     echo _GREETING2;
+        //}
+        //test();
+
+        //echo $x;
+        //unset(_GREETING);
+        //echo _GREETING;
+
+       // echo 10 + 50000 + 68888888888888888888888888888888888, "<br>";
+
+       // echo 50 - 10000000, '<br>';
+
+        //echo 6623355 - 500 + 500 - 10,'<br>'; // left to right
+
+      //  echo 10 * 10 * 10, "<br>";
+
+       // echo 10 / 5 / 2, "<br>";
+
+        // multi and division
+        //echo 10 / 5 * 2, "<br>";
+
+        // add/sub and division/multi
+       //echo 50 + 10 / 2, "<br>"; // multi/division before add/subtract
+
+        //%modulus
+       // echo 7 % 3,"<br>";
+
+        // ** exponential
+        //echo 10 ** 10, "<br>";
+
+        // assignment
+
+        // = equals
+      //  $x = 10;
+        //echo $x, "<br>";
+
+        // +=
+        //$x = $x + 10;
+       // $x +=10;
+        //echo $x, "<br>";
+
+        // -=
+       // $x = $x -10;
+       // $x -=10;
+       // echo $x, "<br>";
+
+        // /=
+       // $x = $x / 10;
+       // $x /=10;
+        //echo $x, "<br>";
+
+        // *=
+        //$x = $x * 10;
+        //$x *= 10;
+        //echo $x, "<br>";
+
+
+
+        //comparison
+        // == equals to = similar value
+        //var_dump(10 == "10");
+        // === identical = similar value and data types
+        //var_dump(10 === "10");
+        // != not equals to = not similar value
+        //var_dump(10 != "10");
+        //var_dump(10 <> "10");
+        // !== not identical = not similar value or not similar data types
+        //var_dump(10 !== "10");
+        // < less than = check if first value is less than right value
+        //var_dump(10 < "10");
+        // > greater than =  check if first value is greater than second value
+        //var_dump(10 > "10");
+        // <= less than or equals to
+        //var_dump(10 <= "10");
+        // >= greater than or equals to
+        //var_dump(10 >= "10");
+        //<=> spaceship = check if first value is less than or equals or greater than second value
+        //var_dump(1 <=> "2");
+        //var_dump(2 <=> "2");
+        //var_dump(3 <=> "2");
+
+        //$x= 10;
+        //echo ++$x, "<br>";
+
+        //$y = 10;
+        //echo $y++, "<br>";
+        //echo $y,"<br>";
+
+        
+
+
+
+        
+
+        
+    
+
+
+
+
+    ?>
+</body>
+</html>
